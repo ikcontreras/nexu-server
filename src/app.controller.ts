c2fe133606ee6f27de3a4be3b541cff9a8989fff
dataflowmaster <iskconsalaz@gmail.com>
2020-05-26 20:33:52 +0200

    Templates with hbs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Get, Controller, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,9 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  render() {
+    const message = this.appService.getHello();
+    return { message };
   }
 }
